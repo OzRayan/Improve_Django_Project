1fa3783dcbfa0e5148479317ce6421f44093a49e
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2018-09-05 14:27:33 +0100

    update

--- menu/forms.py
+++ menu/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import DateField, ModelForm, ValidationError
 from django.forms.extras.widgets import SelectDateWidget
 from django.utils import timezone
 import datetime
