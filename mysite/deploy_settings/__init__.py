32d5e3d122c8524c2da3515cd4d706a59342384f
Fehér Oszkár Rayan <30876326+OzRayan@users.noreply.github.com>
2019-03-06 18:02:49 +0100

    deploy settings update

--- mysite/deploy_settings/__init__.py
+++ mysite/deploy_settings/__init__.py
@@ -1,4 +1,4 @@
-# import dj_database_url
+import dj_database_url
 from mysite.settings import *
 
 
@@ -12,5 +12,5 @@ ALLOWED_HOSTS = [
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
